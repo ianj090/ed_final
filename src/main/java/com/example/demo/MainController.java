66692cef968de94ae51108cdf0eee11a0b031562
Ian <ianjenatz@ufm.edu>
2020-03-18 11:14:33 -0600

    changed controller name

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Main {
+public class MainController {
 
     @GetMapping("/")
     public String greetingForm(Model model) {
