00ecb7b95d63054f4a62b1efb217cc0dba620bab
DavidCorzo <davidcorzo@ufm.edu>
2020-03-20 17:33:20 -0600

    2020-03-20

---
+++ ConsoleAplicationTesting/Class.java
@@ -0,0 +1,31 @@
+
+public class Class {
+    String ClassName;
+    String Teacher;
+    String TA;
+
+    float Credits;
+    String Descripcion;
+
+//    LinkedList<Homework> HomeworkAssignments;
+
+    Class(String ClassName, String Teacher, String TA, float Credits,  String Description){
+        this.ClassName = ClassName;
+        this.Teacher = Teacher;
+        this.TA = TA;
+        this.Credits = Credits;
+        this.Descripcion = Description;
+//      this.HomeworkAssignments   = new LinkedList()
+    }
+
+    public void AddHomeworkAssignment() {
+        /* This method does the following:
+            - Gets homework description from user.
+            - Gets date due, if no date due we will have homework with no date due.
+            - Gets homeworktitle 
+        * */
+
+    }
+
+//    public void
+}
---
+++ ConsoleAplicationTesting/Runner.java
@@ -0,0 +1,12 @@
+
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList obj = new LinkedList();
+//        obj.add(10);
+//        obj.add(11);
+//        obj.add(12);
+//        obj.add(13);
+//        obj.add(14);
+        System.out.println(obj.printL());;
+    }
+}
