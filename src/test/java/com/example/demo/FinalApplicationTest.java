9cc896eb834ef24cbc542359e56aaa83c9fafa53
Ian <ianjenatz@ufm.edu>
2020-05-15 13:18:50 -0600

    Added Unit testing for Grade Manager

--- src/test/java/com/example/demo/FinalApplicationTest.java
+++ src/test/java/com/example/demo/FinalApplicationTest.java
@@ -13,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class FinalApplicationTests {
 
+//      -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
     private CoursesLinkedList courses = new CoursesLinkedList();
     @Test
     void contextLoads() {
@@ -20,10 +22,19 @@ class FinalApplicationTests {
         CoursesLinkedList.toArray();
         Class course1 = new Class();
         course1.ClassName = "Test Class 1";
+        course1.HomeWork = 80.0f;
+        course1.Final = 20.0f;
+        course1.ScoreWanted = 80.0f;
+        course1.Credits = 3.0f;
+        course1.findTotal();
+        course1.findScoreWanted();
         course1.ClassActivities = new AssignmentLinkedList();
         CoursesLinkedList.add(course1);
         Class course2 = new Class();
         course2.ClassName = "Test Class 2";
+        course2.Quiz = 70.0f;
+        course2.Final = 30.0f;
+        course2.ScoreWanted = 100.0f;
         course2.ClassActivities = new AssignmentLinkedList();
         CoursesLinkedList.add(course2);
         Class course3 = new Class();
@@ -43,12 +54,20 @@ class FinalApplicationTests {
         assertEquals("Test Class 2", testArray[2].ClassName);
         assertEquals("Test Class 1<br>Test Class 2<br>", CoursesLinkedList.listNodes());
 
+//      -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
         course1.ClassActivities.toArray();
         Assignment assignment1 = new Assignment();
         assignment1.Title = "Test Assignment 1";
+        assignment1.TypeOfActivity = "homework";
+        assignment1.ScoreOfThisActivity = 50.0f;
         course1.ClassActivities.add(assignment1);
+        course1.findaverages();
+        course1.findScore();
         Assignment assignment2 = new Assignment();
         assignment2.Title = "Test Assignment 2";
+        assignment2.TypeOfActivity = "quiz";
+        assignment2.ScoreOfThisActivity = 100.0f;
         course1.ClassActivities.add(assignment2);
 
         Assignment[] assignmentTestArray = course1.ClassActivities.toArray();
@@ -61,9 +80,26 @@ class FinalApplicationTests {
         assertEquals("Test Assignment 1", assignmentTestArray[0].Title);
         assertEquals("Test Assignment 1<br>", course1.ClassActivities.listNodes());
 
+//      -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        Class[] testArray2 = CoursesLinkedList.toArray();
+        int amount_classes = 0;
+        for (int i=0; i<testArray2.length; i++) {
+            if (testArray2[i] != null) {
+                amount_classes++;
+            }
+        }
+        new ScoreHeap(amount_classes);
+        for (int i=0; i<testArray2.length; i++) {
+            if (testArray2[i] != null) {
+                ScoreHeap.insert(testArray2[i].Score);
+            }
+        }
+
+        assertEquals(40.0, ScoreHeap.getMax());
+        assertEquals(0.0, ScoreHeap.getMin());
+        assertEquals(40.0, ScoreHeap.getAverage());
+        assertEquals(40.0, ScoreHeap.getWeighted());
 
     }
-
-
-
 }
