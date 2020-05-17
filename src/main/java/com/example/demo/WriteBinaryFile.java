0ff9283b04db751993538d5d804ed625c6aed6bd
Ian <ianjenatz@ufm.edu>
2020-05-16 19:58:47 -0600

    fixes and changes, added profiler

--- src/main/java/com/example/demo/WriteBinaryFile.java
+++ src/main/java/com/example/demo/WriteBinaryFile.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class WriteBinaryFile {
     public static void main(String[] args, String file) throws java.io.IOException {
-        FileOutputStream fos = new FileOutputStream(file + ".bin");
+        FileOutputStream fos = new FileOutputStream(file + "classes.bin");
         OutputStreamWriter osw = new OutputStreamWriter(fos);
         BufferedWriter out = new BufferedWriter(osw);
         String text = "I fall;'/.,Ññ to pieces when I'm with you, I fall to pieces\n\nMy cherries and wine, rosemary and thyme\n";
